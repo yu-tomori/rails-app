357f2853fb2ff9078c9eb6c710708046bd1fbce9
yu <peacetomori1118@gmail.com>
2019-03-30 13:56:32 +0900

    meta-tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+ def default_meta_tags
+   {
+      site: 'My Portfolio',
+      title: 'あなたの作品を投稿してみませんか？',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'My Portfolioは作品を投稿して楽しむサービスです',
+      keywords: 'portfolio',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('web-icon1.svg') },
+        { href: image_url('web-icon1.svg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/svg' }
+      ],
+      og: {
+        site_name: :site, # もしくは site_name: :site
+        title: :title, # もしくは title: :title
+        description: :description, # もしくは description: :description
+        type: 'website',
+        url: request.original_url,
+        image: image_url('mono-gradation2.png'),
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary',
+        site: '@peacetomori1118',
+        image: image_url('mono-gradation2.png')
+      }
+    }
+  end
 end
