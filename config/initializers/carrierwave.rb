f85176ea314b86ee30b2ca5ebb89a6db65151fdd
yu <peacetomori1118@gmail.com>
2019-03-31 15:40:10 +0900

    initializer-carrierwave-provider

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider: 'AWS',
+    provider: 'fog/aws',
     aws_access_key_id: 'AKIAUENSDK2S6WMX3OZ',
     aws_secret_access_key: 'Tj9BwvW/2mYnZOiLueFUMSxbQUcm+yMvHj4SYbMn',
     region: 'Asia Pacific (Tokyo)'
